6d344e7b67f0b9478407e6622d653074527bd151
DECIO SANTANA DE AGUIAR <88034016+Decioagu@users.noreply.github.com>
2021-10-30 21:24:17 -0300

    Create ex018.py

---
+++ ex018.py
@@ -0,0 +1,11 @@
+# Seno, Cosseno e Tangente
+import math
+ang = float(input('Digite o valor de um ângulo: '))
+
+sen = math.sin(math.radians(ang))
+cos = math.cos(math.radians(ang))
+tg = math.tan(math.radians(ang))
+
+print('sen \u03B1= {:.2f}'.format(sen))
+print('cos \u03B1= {:.2f}'.format(cos))
+print('tg \u03B1= {:.2f}'.format(tg))
