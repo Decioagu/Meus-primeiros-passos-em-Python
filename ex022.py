a7555cf87650a7b53a8041662af0ea22f2cbc59b
DECIO SANTANA DE AGUIAR <88034016+Decioagu@users.noreply.github.com>
2022-02-05 01:07:14 -0300

    Acrescentado descrição

--- ex022.py
+++ ex022.py
@@ -1,10 +1,14 @@
+# Analisador de Textos
 print('Bom dia!')
 nome = input('Por favor digite seu nome completo: ')
 original = nome
-nome = nome.strip()
+nome = nome.strip() # remove espaço em branco
 print('Seu nome em letras maiúsculas: {}'.format(nome.upper()))
 print('Seu nome em letras minúsculas: {}'.format(nome.lower()))
+
+# conta caractere eliminado espaço em branco
 print('Seu nome "{}" tem {} caracteres.'.format(nome, len(nome) - nome.count(' ')))
-separa = nome.split()
+
+separa = nome.split() # transforma string em lista
 print('Seu primeiro nome "{}" tem {} caracteres.'.format(separa[0], len(separa[0])))
 
